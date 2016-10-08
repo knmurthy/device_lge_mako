df179af52b143b25c5e5bae69dc4e1356d9b6cfc
knmurthy <knmurthy@outlook.com>
2016-10-08 14:33:22 +0530

    Change toolchain to UberTC

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 TARGET_KERNEL_CONFIG := mako_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/mako
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/ubertc-arm-eabi-5.x/bin
 
 TOUCH_BOOST_DEBUG := false
 
